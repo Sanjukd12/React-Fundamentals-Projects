f6d3347f7bdff9ecb050cb9cc07f6d141f3c777c
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-16 23:05:59 +0530

    fetch tours

--- Tours Project/src/App.jsx
+++ Tours Project/src/App.jsx
@@ -36,6 +36,23 @@ const App = () => {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            style={{ marginTop: "2rem" }}
+            className="btn"
+            onClick={() => fetchTours()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main>
