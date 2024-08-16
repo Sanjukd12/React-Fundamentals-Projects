1debe739922f2778e8b5728bb5a350fca7b9026f
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-16 22:05:35 +0530

    useState Hook used

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -11,6 +11,13 @@ function App() {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
