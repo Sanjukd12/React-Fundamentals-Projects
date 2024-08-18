f4ca7116b89d7c26423524b35e7e062fe17b9c03
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-18 12:49:29 +0530

    filter completed

--- Menu Project/src/App.jsx
+++ Menu Project/src/App.jsx
@@ -4,24 +4,28 @@ import Menu from "./Menu";
 import data from "./data"; // Assuming this imports an array of menu items
 import Categories from "./Categories";
 
-// Fix: Use data instead of menu as the variable doesn't exist
-// const tempCategory = data.map((item) => item.category);
-// const tempSet = new Set(tempCategory);
-// const tempItems = ["all", ...tempSet];
-// console.log(tempCategory);
-
+// Create a list of all categories including "all"
 const allCategories = ["all", ...new Set(data.map((item) => item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(data);
   const [categories, setCategories] = useState(allCategories);
-  console.log(categories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(data);
+      return;
+    }
+
+    const newItems = data.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
       <section className="menu">
         <Title text="our menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
