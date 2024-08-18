9824e5fbf8440284203149b435164e2405ec2802
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-18 22:07:47 +0530

    toastify added

--- ColorGenerator Project/src/App.jsx
+++ ColorGenerator Project/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
-  const [colors, setColors] = useState(new Values("#15025").all(10));
+  const [colors, setColors] = useState(new Values("#150025").all(10));
   console.log();
 
   return (
     <main>
       <Form />
-      <ColorList />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
--- ColorGenerator Project/src/ColorList.jsx
+++ ColorGenerator Project/src/ColorList.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import SingleColor from "./SingleColor";
+import { nanoid } from "nanoid";
 
-const ColorList = () => {
+const ColorList = ({ colors }) => {
   return (
-    <div>ColorList</div>
-  )
-}
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
+};
 
-export default ColorList
+export default ColorList;
