5b84d29150233c5c1a1d087b879ceb1e5f557f1a
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-22 22:52:12 +0530

    final

--- navbar/src/Navbar.jsx
+++ navbar/src/Navbar.jsx
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
+
+  useEffect(() => {
+    if (linksRef.current) {
+      const linksHeight = linksRef.current.getBoundingClientRect().height;
+      linksContainerRef.current.style.height = showLinks
+        ? `${linksHeight}px`
+        : "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
-      <div className="nav-center">
+      <div className="nav-header">
         <img src={logo} className="logo" alt="logo" />
         <button className="nav-toggle" onClick={toggleLinks}>
           <FaBars />
         </button>
       </div>
 
-      {showLinks && (
-        <div className="links-container">
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text} </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };
