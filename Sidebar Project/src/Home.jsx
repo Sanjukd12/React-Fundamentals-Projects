b8797dc75422937e6f17648761a629d080d2e888
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-24 12:48:02 +0530

    frame created

---
+++ Sidebar Project/src/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
+
+const Home = () => {
+  const { openSidebar, openModel } = useGlobalContext();
+  console.log(openSidebar);
+
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModel} className="btn">
+        Show Model
+      </button>
+    </main>
+  );
+};
+
+export default Home;
---
+++ Sidebar Project/src/context.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModelOpen, setIsModelOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModel = () => {
+    setIsModelOpen(true);
+  };
+
+  const closeModel = () => {
+    setIsModelOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModelOpen,
+        openModel,
+        closeModel,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
